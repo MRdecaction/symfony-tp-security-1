e3653acb65976301209d019c0ad288bf8d0229de
Piard.Thomas <thomas.piard.etudes@gmail.com>
2020-12-17 12:02:48 +0100

    [CORE] add some stuff

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,13 +18,24 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setEmail("thomas.piard.etudes@gmail.com");
-        $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,
+        $userAdmin = new User();
+        $userAdmin->setEmail("thomas.piard.etudes@gmail.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->passwordEncoder->encodePassword(
+            $userAdmin,
             'admin'
         ));
-        $manager->persist($user);
+        $manager->persist($userAdmin);
+
+        $visiteur = new User();
+        $visiteur->setEmail("Grimskellington8@gmail.com");
+        $visiteur->setRoles(["ROLE_VISITEUR"]);
+        $visiteur->setPassword($this->passwordEncoder->encodePassword(
+            $visiteur,
+            'visiteur'
+        ));
+        $manager->persist($visiteur);
+
         $manager->flush();
     }
 }
